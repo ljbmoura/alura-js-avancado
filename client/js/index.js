58be8d2de12c33507adf7ef08cba720ca036613e
ljbmoura <ljbmoura@gmail.com>
2017-04-29 23:46:09 -0300

    fim aula 1, atividade 3

--- client/js/index.js
+++ client/js/index.js
@@ -5,3 +5,33 @@ var campos = [
 ];
 
 console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener(
+	'submit', 
+	function(event) {
+		event.preventDefault();
+//		alert('oi');
+		
+		var linha = document.createElement('tr');
+		campos.forEach(function(campo) {
+			var coluna = document.createElement('td');
+			coluna.textContent = campo.value;
+			linha.appendChild(coluna);
+		});
+		
+		var colunaVolume = document.createElement('td');
+		colunaVolume.textContent = (campos[1].value * campos[2].value).toFixed(2);
+		
+		linha.appendChild(colunaVolume);
+		
+		tbody.appendChild(linha);
+		
+		campos[0].value = '';
+		campos[1].value = 1;
+		campos[2].value = 0;
+		
+		campos[0].focus();
+	}
+)
