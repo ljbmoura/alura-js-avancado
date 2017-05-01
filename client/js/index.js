278ea0842238f5ffd0939d41d56139b7a135a346
ljbmoura <ljbmoura@gmail.com>
2017-04-30 22:33:42 -0300

    meio capítulo 2

--- client/js/index.js
+++ client/js/index.js
@@ -1,37 +1,29 @@
-var campos = [
-    document.querySelector('#data'),
-    document.querySelector('#valor'),
-    document.querySelector('#quantidade')
-];
+var campos = [ document.querySelector('#data'), document.querySelector('#valor'), document.querySelector('#quantidade') ];
 
 console.log(campos);
 
 var tbody = document.querySelector('table tbody');
 
-document.querySelector('.form').addEventListener(
-	'submit', 
-	function(event) {
-		event.preventDefault();
-//		alert('oi');
-		
-		var linha = document.createElement('tr');
-		campos.forEach(function(campo) {
-			var coluna = document.createElement('td');
-			coluna.textContent = campo.value;
-			linha.appendChild(coluna);
-		});
-		
-		var colunaVolume = document.createElement('td');
-		colunaVolume.textContent = (campos[1].value * campos[2].value).toFixed(2);
-		
-		linha.appendChild(colunaVolume);
-		
-		tbody.appendChild(linha);
-		
-		campos[0].value = '';
-		campos[1].value = 1;
-		campos[2].value = 0;
-		
-		campos[0].focus();
-	}
-)
+document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var linha = document.createElement('tr');
+    campos.forEach(function(campo) {
+	var coluna = document.createElement('td');
+	coluna.textContent = campo.value;
+	linha.appendChild(coluna);
+    });
+
+    var colunaVolume = document.createElement('td');
+    colunaVolume.textContent = (campos[1].value * campos[2].value).toFixed(2);
+
+    linha.appendChild(colunaVolume);
+
+    tbody.appendChild(linha);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+
+    campos[0].focus();
+})
